0a8f6cca44fa448ba3e88ab55c82bbd01321e288
Shrinivas Choudhary <shrinivaschowdhary97@gmail.com>
2020-07-03 22:30:34 +0530

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = ( os.path.join('static'), )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'shrinivaschowdhary97@gmail.com'
-EMAIL_HOST_PASSWORD = 'shrinivasfifa16'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
